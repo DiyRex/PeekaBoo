a9f5ccb3a1a6c1756f7542d8d2ffa076df4fa6e6
DiyRex <75021968+DiyRex@users.noreply.github.com>
2021-12-06 12:02:42 +0530

    Add files via upload

---
+++ Ignore_it/test.ino
@@ -0,0 +1,12 @@
+#include <SoftwareSerial.h>
+SoftwareSerial HC06(10, 11);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
